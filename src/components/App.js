560d0fd410876218e19b0b92189bcc864159ccbe
Naveen Kumar <nvnkr09@gmail.com>
2024-03-07 22:09:36 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,53 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  console.log(todo);
+  console.log(todos);
 
-export default App
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo.trim() !== "") {
+      setTodos([...todos, todo.trim()]);
+      setTodo("");
+    }
+  };
+
+  const handleDelete = (index)=>{
+    const newTodos = todos.filter( (_, idx) => idx !== index);
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="App">
+      <div className="top">
+        <h1>To-Do List</h1>
+        <div className="search">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
+            />
+            <button type="submit">Add Todo</button>
+          </form>
+        </div>
+      </div>
+      <div className="todo-list-container">
+        <ul className="todo-list">
+        {todos.map((todo, index) => (
+            <li>
+              <span>{todo}</span>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+              {index}
+            </li>
+        ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
